ea418520fd6a703d5638707ff3694eb60fff7af9
Anis <anisberchem1189@gmail.com>
2020-10-04 17:59:14 -0700

    finition du carousel

--- public/js/main.js
+++ public/js/main.js
@@ -119,6 +119,29 @@ window.addEventListener("scroll", () => {
     }
 })
 
+const buttonsWrapper = document.querySelector(".map");
+const slides = document.querySelector(".inner");
+
+buttonsWrapper.addEventListener("click", e => {
+  if (e.target.nodeName === "BUTTON") {
+    Array.from(buttonsWrapper.children).forEach(item =>
+      item.classList.remove("active")
+    );
+    if (e.target.classList.contains("first")) {
+      slides.style.transform = "translateX(-10%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains("second")) {
+      slides.style.transform = "translateX(-30%)";
+      e.target.classList.add("active");
+    } else if (e.target.classList.contains('third')){
+      slides.style.transform = 'translatex(-40%)';
+      e.target.classList.add('active');
+    } else if (e.target.classList.contains('quatre')){
+        slides.style.transform = 'translatex(-60%)';
+        e.target.classList.add('active');
+      } 
+  }
+});
 
 
 
